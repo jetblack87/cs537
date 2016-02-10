64da25f203b27951fb461166db563d994d45c84a
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2016-02-10 10:55:50 -0500

    Assignment 5 - currenty does not work. Will attempt to start over and take little steps to work forward.

--- mwa29_a5/vshdrcube.glsl
+++ mwa29_a5/vshdrcube.glsl
@@ -2,11 +2,11 @@
 
 attribute vec4 vPosition, vColor;
 varying vec4 color;
-uniform mat4 matrix;
+uniform mat4 ModelView;
 
 void
 main()
 {
-    gl_Position = matrix * vPosition;
+    gl_Position = ModelView * vPosition;
     color = vColor;
 }
