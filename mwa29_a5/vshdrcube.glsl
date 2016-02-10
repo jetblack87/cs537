e312c30cf4571fe207a33eea05dd1630963af007
jetblack87 <mwa29@drexel.edu>
2016-02-10 15:00:41 -0500

    Attempting to get orthgraphic projection working - not working perfectly...

--- mwa29_a5/vshdrcube.glsl
+++ mwa29_a5/vshdrcube.glsl
@@ -2,11 +2,10 @@
 
 attribute vec4 vPosition, vColor;
 varying vec4 color;
-uniform mat4 matrix;
-
+uniform mat4 matrix, Projection;
 void
 main()
 {
-    gl_Position = matrix * vPosition;
+    gl_Position = Projection * matrix * vPosition;
     color = vColor;
 }
