329bff29e325824844eb6ea5464ca1999c1983f6
jetblack87 <mwa29@drexel.edu>
2016-02-10 12:24:46 -0500

    After much toil, I was able to get the parser working. Epiphany: the vector.size() is not the same as sizeof().

--- mwa29_a5/vshdrcube.glsl
+++ mwa29_a5/vshdrcube.glsl
@@ -2,11 +2,11 @@
 
 attribute vec4 vPosition, vColor;
 varying vec4 color;
-uniform mat4 ModelView;
+uniform mat4 matrix;
 
 void
 main()
 {
-    gl_Position = ModelView * vPosition;
+    gl_Position = matrix * vPosition;
     color = vColor;
 }
