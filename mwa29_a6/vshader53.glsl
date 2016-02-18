32b1ba45f9e215b712aa4905443d029fbc052e90
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2016-02-17 20:10:13 -0500

    Adding two light sources to shaders.

--- mwa29_a6/vshader53.glsl
+++ mwa29_a6/vshader53.glsl
@@ -1,39 +1,88 @@
-attribute  vec4 vPosition;
-attribute  vec3 vNormal;
-varying vec4 color;
+#version 120
 
-uniform vec4 AmbientProduct, DiffuseProduct, SpecularProduct;
+attribute vec4 vPosition;
+attribute vec3 vNormal;
+varying   vec4 color;
+
 uniform mat4 ModelView;
 uniform mat4 Projection;
-uniform vec4 LightPosition;
+
+uniform vec4 MaterialAmbient;
+uniform vec4 MaterialDiffuse;
+uniform vec4 MaterialSpecular;
+uniform vec4 Light0Position;
+uniform vec4 Light0Ambient;
+uniform vec4 Light0Diffuse;
+uniform vec4 Light0Specular;
+uniform vec4 Light1Position;
+uniform vec4 Light1Ambient;
+uniform vec4 Light1Diffuse;
+uniform vec4 Light1Specular;
 uniform float Shininess;
+
 void main()
 {
-    // Transform vertex  position into eye coordinates
-    vec3 pos = (ModelView * vPosition).xyz;
-	
-    vec3 L = normalize( (ModelView*LightPosition).xyz - pos );
-    vec3 E = normalize( -pos );
-    vec3 H = normalize( L + E );
+  gl_Position = Projection * ModelView * vPosition;
 
-    // Transform vertex normal into eye coordinates
-    vec3 N = normalize( ModelView*vec4(vNormal, 0.0) ).xyz;
+  // Transform vertex  position into eye coordinates
+  vec3 pos = (ModelView * vPosition).xyz;
 
-    // Compute terms in the illumination equation
-    vec4 ambient = AmbientProduct;
+  // Transform vertex normal into eye coordinates
+  vec3 N = normalize( ModelView*vec4(vNormal, 0.0) ).xyz;
 
-    float Kd = max( dot(L, N), 0.0 );
-    vec4  diffuse = Kd*DiffuseProduct;
+  vec4 ambient = vec4(0.0);
+  vec4 diffuse = vec4(0.0);
+  vec4 specular = vec4(0.0);
 
-    float Ks = pow( max(dot(N, H), 0.0), Shininess );
-    vec4  specular = Ks * SpecularProduct;
-    
-    if( dot(L, N) < 0.0 ) {
-	specular = vec4(0.0, 0.0, 0.0, 1.0);
-    } 
 
-    gl_Position = Projection * ModelView * vPosition;
+  // Light0
+  vec4 ambient0_product  = Light0Ambient  * MaterialAmbient;
+  vec4 diffuse0_product  = Light0Diffuse  * MaterialDiffuse;
+  vec4 specular0_product = Light0Specular * MaterialSpecular;
 
-    color = ambient + diffuse + specular;
-    color.a = 1.0;
+  vec3 L0 = normalize( (ModelView*Light0Position).xyz - pos );
+  vec3 E0 = normalize( -pos );
+  vec3 H0 = normalize( L0 + E0 );
+
+  // Compute terms in the illumination equation
+  ambient += ambient0_product;
+
+  float Kd0 = max( dot(L0, N), 0.0 );
+  diffuse += Kd0*diffuse0_product;
+
+  float Ks0 = pow( max(dot(N, H0), 0.0), Shininess );
+  specular += Ks0 * specular0_product;
+
+  if( dot(L0, N) < 0.0 ) {
+    specular = vec4(0.0, 0.0, 0.0, 1.0);
+  }
+  // End Light0
+
+  // Light1
+  vec4 ambient1_product  = Light1Ambient  * MaterialAmbient;
+  vec4 diffuse1_product  = Light1Diffuse  * MaterialDiffuse;
+  vec4 specular1_product = Light1Specular * MaterialSpecular;
+
+  vec3 L1 = normalize( (ModelView*Light1Position).xyz - pos );
+  vec3 E1 = normalize( -pos );
+  vec3 H1 = normalize( L1 + E1 );
+
+  // Compute terms in the illumination equation
+  ambient += ambient1_product;
+
+  float Kd1 = max( dot(L1, N), 0.0 );
+  diffuse += Kd1 * diffuse1_product;
+
+  float Ks1 = pow( max(dot(N, H1), 0.0), Shininess );
+  specular += Ks1 * specular1_product;
+
+  if( dot(L1, N) < 0.0 ) {
+    specular = vec4(0.0, 0.0, 0.0, 1.0);
+  }
+  // End Light1
+
+
+
+  color = ambient + diffuse + specular;
+  color.a = 1.0;
 }
--- mwa29_a6/vshader56.glsl
+++ mwa29_a6/vshader56.glsl
@@ -4,20 +4,26 @@ attribute   vec3 vNormal;
 // output values that will be interpretated per-fragment
 varying  vec3 fN;
 varying  vec3 fE;
-varying  vec3 fL;
+varying  vec3 fL0;
+varying  vec3 fL1;
 
 uniform mat4 ModelView;
-uniform vec4 LightPosition;
+uniform vec4 Light0Position;
+uniform vec4 Light1Position;
 uniform mat4 Projection;
 
 void main()
 {
     fN = vNormal;
     fE = vPosition.xyz;
-    fL = (ModelView*LightPosition).xyz;
+    fL0 = (ModelView*Light0Position).xyz;
+    fL1 = (ModelView*Light1Position).xyz;
     
-    if( LightPosition.w != 0.0 ) {
-	fL = LightPosition.xyz - vPosition.xyz;
+    if( Light0Position.w != 0.0 ) {
+	fL0 = Light0Position.xyz - vPosition.xyz;
+    }
+    if( Light1Position.w != 0.0 ) {
+	fL1 = Light1Position.xyz - vPosition.xyz;
     }
 
     gl_Position = Projection*ModelView*vPosition;
