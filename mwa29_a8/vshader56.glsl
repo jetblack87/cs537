e1f711183a1c7d04fa0c96939aa1d8c3c8e20846
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2016-02-26 20:46:29 -0500

    Initial add of assignment 8 based on assignment 7.

---
+++ mwa29_a8/vshader56.glsl
@@ -0,0 +1,41 @@
+attribute   vec4 vPosition;
+attribute   vec3 vNormal;
+
+// output values that will be interpretated per-fragment
+varying  vec3 fN;
+varying  vec3 fE;
+varying  vec3 fL;
+varying  vec4 color;
+varying  float SelectedControlPoint;
+
+uniform mat4 ModelView;
+uniform vec4 LightPosition;
+uniform mat4 Projection;
+uniform bool FlatShading;
+uniform vec4 MaterialDiffuse;
+uniform vec4 LightDiffuse;
+
+void main()
+{
+    fN = vNormal;
+    fE = vPosition.xyz;
+    fL = (ModelView*LightPosition).xyz;
+    
+    if( LightPosition.w != 0.0 ) {
+	fL = LightPosition.xyz - vPosition.xyz;
+    }
+
+    if (FlatShading) {
+        color = (MaterialDiffuse * LightDiffuse *
+                           max(dot(vec4(vNormal,1.0),
+                                   normalize(LightPosition)),
+                               0.0));
+    }
+
+    gl_Position = Projection*ModelView*vPosition;
+    if (vPosition.w == 1.2345) {
+      SelectedControlPoint = 1.0;
+    } else {
+      SelectedControlPoint = 0.0;
+    }
+}
