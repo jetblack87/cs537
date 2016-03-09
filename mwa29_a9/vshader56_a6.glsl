7445e0cf864d94445c5117f928bc889720ed8201
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2016-03-09 16:49:01 -0500

    Working on 3d texture... currently have the brick in the vertex shader... doesn't look so great.

--- mwa29_a9/vshader56_a6.glsl
+++ mwa29_a9/vshader56_a6.glsl
@@ -4,38 +4,39 @@ attribute   vec3 vNormal;
 // output values that will be interpretated per-fragment
 varying  vec3 fN;
 varying  vec3 fE;
-varying  vec3 fL;
-varying  vec4 color;
-varying  float SelectedControlPoint;
+varying  vec3 fL0;
+varying  vec3 fL1;
 
 uniform mat4 ModelView;
-uniform vec4 LightPosition;
+uniform vec4 Light0Position;
+uniform vec4 Light1Position;
 uniform mat4 Projection;
-uniform bool FlatShading;
-uniform vec4 MaterialDiffuse;
-uniform vec4 LightDiffuse;
+
+varying vec3 color;
+uniform vec3  BrickColor, MortarColor; 
+uniform vec2  BrickSize,  BrickPct;
+//varying vec2  MCposition; 
 
 void main()
 {
+    vec2  position, useBrick;         
+    position = vec2(vPosition.x, vPosition.y) / BrickSize;     
+    if (fract(position.y * 0.5) > 0.5)    position.x += 0.5;     
+    position = fract(position);     
+    useBrick = step(position, BrickPct);     
+    color  = mix(MortarColor, BrickColor, useBrick.x * useBrick.y);
+
     fN = vNormal;
     fE = vPosition.xyz;
-    fL = (ModelView*LightPosition).xyz;
+    fL0 = (ModelView*Light0Position).xyz;
+    fL1 = (ModelView*Light1Position).xyz;
     
-    if( LightPosition.w != 0.0 ) {
-	fL = LightPosition.xyz - vPosition.xyz;
+    if( Light0Position.w != 0.0 ) {
+	fL0 = Light0Position.xyz - vPosition.xyz;
     }
-
-    if (FlatShading) {
-        color = (MaterialDiffuse * LightDiffuse *
-                           max(dot(vec4(vNormal,1.0),
-                                   normalize(LightPosition)),
-                               0.0));
+    if( Light1Position.w != 0.0 ) {
+	fL1 = Light1Position.xyz - vPosition.xyz;
     }
 
     gl_Position = Projection*ModelView*vPosition;
-    if (vPosition.w == 1.2345) {
-      SelectedControlPoint = 1.0;
-    } else {
-      SelectedControlPoint = 0.0;
-    }
 }
